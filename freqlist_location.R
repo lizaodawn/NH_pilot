a1b4bf19c9600877cc9f789401a89f9cbbc8ace4
lizaodawn <lizaozhuang@gmail.com>
2023-06-14 03:47:47 +0200

    r script importing corpus in txt

--- freqlist_location.R
+++ freqlist_location.R
@@ -12,6 +12,7 @@ library(readr)
 library(stringr)
 fpath_target <- here("geotext_indianregion.csv")
 fpath_ref <- here("wholebooktext.csv")
+fname
 # Read the CSV file
 data_target <- read_csv(fpath_target)
 data_ref <- read_csv(fpath_ref)
@@ -20,8 +21,12 @@ data_ref <- read_csv(fpath_ref)
 # Preprocess the "Plain_text" column
 data_target$Text <- tolower(data_target$Text)
 data_ref$Text <- tolower(data_ref$Text)
-flist_target <- freqlist(data_target$Text, as_text = TRUE) %>% print()
-flist_ref <- freqlist(data_ref$Text, as_text = TRUE) %>% print()
+
+corpus_target <- unique(data_target$Text)
+corpus_ref <- unique(data_ref$Text)
+
+flist_target <- freqlist(corpus_target, as_text = TRUE) %>% print()
+flist_ref <- freqlist(corpus_ref, as_text = TRUE) %>% print()
 
 # calculate scores
 scores_kw <- assoc_scores(flist_target, flist_ref)
@@ -42,10 +47,17 @@ top_scores_kw %>%
   print(sort_order = "G_signed")
 
 
-corpus_folder <- here("geotext_whole.csv")
-fnames_geo <- get_fnames(corpus_folder) %>% 
-  keep_re("[.]csv")
-coocs <- surf_cooc(data_ref$Text, "(?xi)  ^ india $")
+
+
+
+corpus_folder <- here("geotext_whole")
+fnames_geotext <- get_fnames(corpus_folder) %>% 
+  keep_re("[.]txt")
+
+print(fnames_BASE, 10, hide_path = corpus_folder)
+
+coocs <- fnames_BASE %>% 
+  surf_cooc("(?xi)  ^ india $")
 coocs$target_freqlist
 coocs$ref_freqlist
 
@@ -54,3 +66,6 @@ scores_colloc <- assoc_scores(coocs)
 
 # print scores, sorted by PMI
 print(scores_colloc, sort_order = "PMI")
+
+# print scores, sorted by G_signed
+print(scores_colloc, sort_order = "G_signed")
