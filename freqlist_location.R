dc23ea808e24aee022a1faefac065a460390a6b4
lizaodawn <lizaozhuang@gmail.com>
2023-06-14 19:51:38 +0200

    0614 update

--- freqlist_location.R
+++ freqlist_location.R
@@ -8,6 +8,23 @@ library('tidyverse')
 library('mclm')
 library('here')
 library('kableExtra')
+library(ggplot2)
+library(dplyr)
+
+data <- read.csv("geotext_whole.csv")
+
+summary_data <- data %>%
+  group_by(ToposText_ID, Place_Name) %>%
+  summarise(Count = n()) %>%
+  arrange(desc(Count)) %>%
+  top_n(10, Count) %>% print()
+
+ggplot(summary_data, aes(x = Count, y = reorder(Place_Name, Count))) +
+  geom_bar(stat = "identity", fill = "steelblue") +
+  labs(x = "Count", y = "Place_Name") +
+  ggtitle("Top 30 Most Referred Location Names") +
+  facet_wrap(~ Place_Name, ncol = 1, scales = "free_y") +
+  theme(axis.text.y = element_text(size = 8, hjust = 0.5))
 
 
 fpath_target <- here("geotext_indianregion.csv")
@@ -84,8 +101,25 @@ scores_kw <- assoc_scores(flist_target, flist_ref)
 # print scores, sorted by PMI
 print(scores_kw, sort_order = "PMI")
 
+# print scores, sorted by G_signed
+print(scores_kw, sort_order = "G_signed")
+
+top_scores_kw <- scores_kw %>% 
+  filter(PMI >= 2 & G_signed >= 2)
+
+# print top_scores_kw, sorted by PMI
+top_scores_kw %>%
+  print(sort_order = "PMI")
+
+# print top_scores_kw, sorted by G_signed
+top_scores_kw %>%
+  print(sort_order = "G_signed")
+
 coocs <- fnames_wholetext %>% 
-  surf_cooc("(?xi)  ^ india $")
+  surf_cooc("(?xi)  ^ india $", 
+            re_token_splitter = r"--[(?xi)    \s+   ]--", # whitespace as token splitter
+            re_token_transf_in = "[[:punct:]]", # Match punctuation marks
+            token_transf_out = "")
 coocs$target_freqlist
 coocs$ref_freqlist
 
@@ -98,5 +132,32 @@ print(scores_colloc, sort_order = "PMI")
 # print scores, sorted by G_signed
 print(scores_colloc, sort_order = "G_signed")
 
+top_scores_colloc <- scores_colloc %>% 
+  filter(PMI >= 2 & G_signed >= 2)
 
+# print top_scores_colloc, sorted by PMI
+top_scores_colloc %>%
+  print(sort_order = "PMI")
+
+# print top_scores_colloc, sorted by G_signed
+top_scores_colloc %>%
+  print(sort_order = "G_signed")
+
+top_scores_colloc %>% # also valid for top_scores_kw
+  as_tibble() %>%
+  select(type, a, PMI, G_signed) %>% # select 4 columns
+  arrange(desc(G_signed)) %>%        # sort by G_signed (descending)  
+  head(30) %>%                       # select top 30 rows
+  kbl(col.names = c("Type", "Frequency", "PMI", r"(Signed $G^2$)")) %>% 
+  kable_minimal() %>% 
+  scroll_box(height = "400px")
+
+top_scores_kw %>% # also valid for top_scores_colloc
+  as_tibble() %>%
+  select(type, a, PMI, G_signed) %>% # select 4 columns
+  arrange(desc(PMI)) %>%             # sort by PMI (descending) 
+  head(30) %>%                       # select top 30 rows
+  kbl(col.names = c("Type", "Frequency", "PMI", r"(Signed $G^2$)")) %>% 
+  kable_minimal() %>% 
+  scroll_box(height = "400px")
 
